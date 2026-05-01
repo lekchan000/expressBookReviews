644bf320f37cc5c226425db19014d485e1274c52
lekchan000 <lekchan000@gmail.com>
2026-05-01 12:57:47 +0700

    Refactor API endpoints to remove redundant 'customer' prefix and update session handling

--- final_project/index.js
+++ final_project/index.js
@@ -8,10 +8,8 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
-
-// Middleware to authenticate requests to "/customer" endpoint
-app.use("/customer/auth/*", function auth(req, res, next) {
+app.use("/", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
+app.use("/auth/*", function auth(req, res, next) {
     // Check if user is logged in and has valid access token
     if (req.session.authorization) {
         let token = req.session.authorization['accessToken'];
@@ -31,8 +29,7 @@ app.use("/customer/auth/*", function auth(req, res, next) {
 
 const PORT = 5000;
 
-app.use("/customer", customer_routes);
+app.use("/", customer_routes);
 app.use("/", genl_routes);
 
 app.listen(PORT, () => console.log("Server is running"));
-
